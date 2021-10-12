5f25fb2063eb792b876888f9706c976073acf9c5
Qinyu Zhu <zz635186056@gmail.com>
2021-10-12 13:22:08 -0700

    connect server with client

--- server/index.js
+++ server/index.js
@@ -1,12 +1,12 @@
 const express = require('express');
+const path = require('path');
 const app = express()
 const port = 3000
 
 app.use(express.json());
+app.use(express.static(path.join(__dirname, '/../client/dist')));
 
-app.get('/', (req, res) => {
-  res.send('Hello world')
-})
+
 
 
 app.listen(port, () => {
