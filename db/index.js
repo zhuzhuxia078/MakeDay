7c593d446ebbc69327d669ace335a05c8866e042
Qinyu Zhu <zz635186056@gmail.com>
2021-10-13 10:31:17 -0700

    implement GET route

--- db/index.js
+++ db/index.js
@@ -1,12 +1,22 @@
-//const mysql = require('mysql');
-import mysql from 'mysql';
-import mysqlConfig from './config.js';
-//const mysqlConfig = require('./config.js');
+const mysql = require('mysql');
+//import mysql from 'mysql';
+//import mysqlConfig from './config.js';
+const mysqlConfig = require('./config.js');
 
 const db = mysql.createConnection(mysqlConfig);
 
-/* query functions */
+const getBoxes = function(callback) {
+  const query = 'SELECT * FROM my_box';
+  db.query(query, (error, rows) => {
+    if (error) {
+      throw error;
+    } else {
+      console.log('getBoxes success in db')
+      callback(null, rows);
+    }
+  })
+}
 
 module.exports = {
-  /* query function */
+  getBoxes
 }
--- server/index.js
+++ server/index.js
@@ -1,12 +1,23 @@
 const express = require('express');
 const path = require('path');
+const db = require('../db')
 const app = express()
 const port = 3000
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '/../client/dist')));
 
-
+// routes
+  // ---- /boxes
+app.get('/boxes', (req, res) => {
+  db.getBoxes((error, rows) => {
+    if (error) {
+      throw error;
+    } else {
+      res.json(rows);
+    }
+  });
+});
 
 
 app.listen(port, () => {
