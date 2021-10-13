966d74938ca9db61ae88553b4b4f2ce4d52cd92b
Qinyu Zhu <zz635186056@gmail.com>
2021-10-12 19:26:32 -0700

    schema

--- schema.sql
+++ schema.sql
@@ -1,15 +1,19 @@
 -- to enter mysql
-   mysql -u root
+  -- mysql -u root
 
+-- to execute this schema
+  -- mysql < schema.sql
 -- schema
 
 DROP DATABASE IF EXISTS makeDay;
 
 CREATE DATABASE makeDay;
 
+USE makeDay;
+
 CREATE TABLE my_box (
-  id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-  box_name VARCHAR NOT NULL,
-  date DATE NOT NULL,
-  cost DECIMAL(6,2) NOT NULL,
+  id int NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  box_name VARCHAR(50) NOT NULL,
+  date date,
+  amount decimal(10, 2) NOT NULL
 );
